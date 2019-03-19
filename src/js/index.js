abce8f0323f0398f934775f30515c88f586a1e08
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-03-19 19:20:57 +0100

    Creating API call with axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
-// Global app controller
-import x from './test';
-const num = 23;
+//Key 9dc876f16571f396ea5d33e7e2758946
+//https://www.food2fork.com/api/search
 
-console.log(`I imported ${x} TEST V2 var x is ${num}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const key = '9dc876f16571f396ea5d33e7e2758946';
+
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error) {
+        alert(error);
+    }
+    
+}
+
+getResults('burger');
