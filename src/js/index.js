112ed58c8a69b9ba852563cafe784524f8df160e
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-03-22 20:13:57 +0100

    Building recipe model methods

--- src/js/index.js
+++ src/js/index.js
@@ -17,8 +17,8 @@ const state = {};
  */
 const controlSearch = async () => {
     // 1 Get query from view
-    const query = searchView.getInput();
-    console.log(query);
+    //const query = searchView.getInput();
+    const query = 'pizza';
 
     if(query) {
         // 2 New search object and add to state
@@ -29,17 +29,28 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4 Search for recipe
-        await state.search.getResults();
+        try{
+            // 4 Search for recipe
+            await state.search.getResults();
 
-        // 5 Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5 Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something went wrong with the search... index.js');
+            clearLoader();
+        }
     }
 
 }
 
 elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+//just testing
+window.addEventListener('load', e => {
     e.preventDefault();
     controlSearch();
 });
@@ -60,6 +71,33 @@ elements.searchResPages.addEventListener('click', e => {
  * RECIPE CONTROLLER
  */
 
- const r = new Recipe(46956);
- r.getRecipe();
- console.log(r);
+const controlRecipe = async () => {
+    //Get id from url
+    const id = window.location.hash.replace('#', '');    
+    if (id) {
+        //Prepare UI for changes
+
+        //Create new recipe object
+        state.recipe = new Recipe(id);
+
+        //TESTING
+        window.r = state.recipe;
+
+        try {
+            //Get recipe data
+            await state.recipe.getRecipe();
+
+            //Calc servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            //Render recipe
+            console.log(state.recipe);
+        } catch (err) {
+            alert('Error processing recipe!');
+        }
+    }
+};
+
+['hashchange', 'load'].forEach( event => window.addEventListener(event, controlRecipe));
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -15,6 +15,7 @@ export default class Search {
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
             console.log(this.result);
+            
         } catch(error) {
             alert(error);
         }
