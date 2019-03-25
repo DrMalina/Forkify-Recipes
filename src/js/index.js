a0fff3481c985929744242591eff735b400773e4
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-03-25 14:24:07 +0100

    Recipe view UI with fraction.js

--- src/js/index.js
+++ src/js/index.js
@@ -73,6 +73,9 @@ const controlRecipe = async () => {
         recipeView.clearRecipe();
         renderLoader(elements.recipe);
 
+        //Highlight selected search items
+        if(state.search) searchView.highlightSelected(id);
+
         //Create new recipe object
         state.recipe = new Recipe(id);
 
